21774989ba8c354d9883768fa40ed0b9041c7fba
GitHubCongHua <937274446@qq.com>
2017-05-09 17:34:59 +0800

    Update sim-hiera-task.py

--- sim-hiera-task.py
+++ sim-hiera-task.py
@@ -6,11 +6,7 @@ from Node import Node
 from Agent import Agent
 
 '''
-这个例子是，多agent任务分层，先分为简单的层次：
-root
- |
-go to T1 , go to T2
-下面就是原始行为：四个方向（上，下，左，右）
+这个例子是，多agent任务分层，先分层次
 
 两个agent开始位置（先不随机，直接固定吧！），T1和T2位置固定
 '''
@@ -29,7 +25,11 @@ a1 = Agent(1, (6, 3))
 a2 = Agent(2, (7, 3))
 agent_list = [a1, a2]
 t1_end = (0, 0)
+t1_trash = 1
 t2_end = (7, 7)
+t2_trash = 1
+dump_end = (0, 7)
+dump_trash = [0, 0]
 gamma = 0.8
 alpha = 0.7
 epsilon = 0.9
@@ -37,15 +37,59 @@ epsilon = 0.9
 #  任务分层, 引用定义的Node类
 #  node(type1, name, flag, terminal)，+children属性
 M0 = Node('c-sub-task', 'root', 0, '')
-M1 = Node('sub-task', 'go to T1', 0, t1_end)
-M2 = Node('sub-task', 'go to T2', 0, t2_end)
+M1 = Node('sub-task', 'collect trash at t1', 0, '')
+M2 = Node('sub-task', 'collect trash at t2', 0, '')
+M3 = Node('sub-task', 'navigate to t1', 0, t1_end)
+M4 = Node('sub-task', 'navigate to t2', 0, t2_end)
+M5 = Node('sub-task', 'navigate to dump', 0, dump_end)
+P1 = Node('action_p', 'pick', '', '')
+P2 = Node('action_p', 'put', '', '')
 C1 = Node('action', (-1, 0), '', '')
 C2 = Node('action', (1, 0), '', '')
 C3 = Node('action', (0, -1), '', '')
 C4 = Node('action', (0, 1), '', '')
 M0.append_children((M1, M2))
-M1.append_children((C1, C2, C3, C4))
-M2.append_children((C1, C2, C3, C4))
+M1.append_children((M3, M5, P1, P2))
+M2.append_children((M4, M5, P1, P2))
+M3.append_children((C1, C2, C3, C4))
+M4.append_children((C1, C2, C3, C4))
+M5.append_children((C1, C2, C3, C4))
+
+
+# 如果任务结束，返回True；否则返回False
+def terminal(task, state):
+    if task.name == 'root':
+        if dump_trash[0] == 1 and dump_trash[1] == 1:
+            return True
+        else:
+            return False
+    elif task.name == 'collect trash at t1':
+        if dump_trash[0] == 1:
+            return True
+        else:
+            return False
+    elif task.name == 'collect trash at t2':
+        if dump_trash[1] == 1:
+            return True
+        else:
+            return False
+    elif task.name == 'navigate to t1':
+        if state == t1_end:
+            return True
+        else:
+            return False
+    elif task.name == 'navigate to t2':
+        if state == t2_end:
+            return True
+        else:
+            return False
+    elif task.name == 'navigate to dump':
+        if state == dump_end:
+            return True
+        else:
+            return False
+    else:
+        print('error')
 
 
 def vector_add(a, b):
@@ -64,8 +108,8 @@ def c_hrl(agent, task, state):
     # seq is a sequence store states visited and actions of other agents
     seq = []
     next_state = None
-    if task.type == 'action':
 
+    if task.type == 'action':
         destination = task.parent.terminal
         next_state = vector_add(state, task.name)
         if (next_state[0] not in range(8)) or (next_state[1] not in range(8)) \
@@ -89,7 +133,7 @@ def c_hrl(agent, task, state):
                     seq.append(agents.u_action[0])
         print(seq)
     else:
-        while task.flag == 0:
+        while not terminal(task, state):
             if task.type == 'c-sub-task':
                 sub_task = random.choice(task.get_children())
                 child_seq, next_state = c_hrl(agent, sub_task, state)
